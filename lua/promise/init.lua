323f2c9228557e0bb569dd7de3e563344c748735
notomo <notomo.motono@gmail.com>
2021-11-22 00:36:10 +0900

    Fix require for embed

--- lua/promise/init.lua
+++ lua/promise/init.lua
@@ -1,4 +1,28 @@
-local PackedValue = require("promise.packed_value")
+local PackedValue = {}
+PackedValue.__index = PackedValue
+
+function PackedValue.new(...)
+  local values = vim.F.pack_len(...)
+  local tbl = {_values = values}
+  return setmetatable(tbl, PackedValue)
+end
+
+function PackedValue.pcall(self, f)
+  local ok_and_value = function(ok, ...)
+    return ok, PackedValue.new(...)
+  end
+  return ok_and_value(pcall(f, self:unpack()))
+end
+
+function PackedValue.unpack(self)
+  return vim.F.unpack_len(self._values)
+end
+
+function PackedValue.first(self)
+  local first = self:unpack()
+  return first
+end
+
 local vim = vim
 
 local Promise = {}
